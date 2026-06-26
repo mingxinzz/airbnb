871accfbe4a24253d224b5906d9e0becc7579740
hgy <m17878660695@163.com>
2026-06-26 09:39:16 +0800

    fix:修复任务日志

--- backend/src/main/java/cn/guet/airbnb/mapper/PipelineTaskLogMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.guet.airbnb.mapper;
-
-import cn.guet.airbnb.entity.PipelineTaskLog;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PipelineTaskLogMapper extends BaseMapper<PipelineTaskLog> {
-}
